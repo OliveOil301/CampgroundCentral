dec3f34026f57e5041ad254abb00569f824341fa
jacob <jbernard@wpi.edu>
2021-07-07 18:05:31 -0400

    did a little more work on the available/reserved days stuff. Still need to decide how to do it.

--- src/main/java/Camping/Site.java
+++ src/main/java/Camping/Site.java
@@ -8,7 +8,7 @@ public class Site {
     private String siteType;
     private String groupName;
     private ArrayList<Reservation> listOfReservations;
-    private ArrayList<LocalDate> reservedDates;
+    private ArrayList<LocalDate> availableDates;
 
     public Site( String groupName, String siteName, String siteType) {
         this.siteName = siteName;
@@ -20,14 +20,14 @@ public class Site {
         this.siteName = siteName;
         this.siteType = siteType;
         this.listOfReservations = listOfReservations;
-        loadReservedDates();
+        loadAvailableDates();
         //need to read a csv for the availableSiteTypes assignment here.
     }
 
     public Site(String siteName, ArrayList<Reservation> listOfReservations) {
         this.siteName = siteName;
         this.listOfReservations = listOfReservations;
-        loadReservedDates();
+        loadAvailableDates();
         //need to read a csv for the availableSiteTypes assignment here.
 
     }
@@ -59,12 +59,7 @@ public class Site {
         this.listOfReservations.add(r);
 
         //This stuff below makes sure the reservedDates list gets updated when a reservation is added.
-        LocalDate reservedDay = r.getStartDate();
-        while(!reservedDay.isEqual(r.getEndDate())){
-            this.reservedDates.add(reservedDay);
-            reservedDay = reservedDay.plusDays(1);
-        }
-        this.reservedDates.add(r.getEndDate());
+        //TODO: remake this part
 
     }
 
@@ -74,20 +69,33 @@ public class Site {
 
 
     /**
-     * loadReservedDates takes the list of reservations in this object and generates a list of reserved dates from it.
+     * loadReservedDates takes the list of reservations in this object and generates a list of available dates from it.
      * This makes creating new reservations and searching for available days much easier.
+     * This loads available days 500 days ahead of the current day
      */
-    private void loadReservedDates(){
-        ArrayList<LocalDate> dates = new ArrayList<LocalDate>();
+    private void loadAvailableDates(){
+        ArrayList<LocalDate> reservedDates = new ArrayList<LocalDate>();
         for (Reservation r: this.listOfReservations) {
             LocalDate reservedDay = r.getStartDate();
             while(!reservedDay.isEqual(r.getEndDate())){
-                dates.add(reservedDay);
+                reservedDates.add(reservedDay);
                 reservedDay = reservedDay.plusDays(1);
             }
-            dates.add(r.getEndDate());
+            reservedDates.add(r.getEndDate());
         }
-        this.reservedDates = dates;
+
+        ArrayList<LocalDate> availableDates = new ArrayList<LocalDate>();
+        LocalDate dateToEndOn = LocalDate.now().plusDays(500);
+        LocalDate now = LocalDate.now();
+        while(!now.isEqual(dateToEndOn)){
+            for (LocalDate d:reservedDates) {
+                if(!now.isEqual(d)){
+                    availableDates.add(now);
+                }
+            }
+        }
+
+        this.availableDates = availableDates;
     }
 
     private void getReservableDates(LocalDate endingDay){
