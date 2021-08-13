fcd7553cfba1a9dbee08cfa4462c2e744001f972
jacob <jbernard@wpi.edu>
2021-08-13 10:00:57 -0400

    Did some work on the Reservation Wizard pane Started a little on the controller

--- src/main/java/Controllers/ReservationWizardPaneController.java
+++ src/main/java/Controllers/ReservationWizardPaneController.java
@@ -1,4 +1,21 @@
 package main.java.Controllers;
 
+import com.jfoenix.controls.JFXDatePicker;
+import javafx.fxml.FXML;
+
 public class ReservationWizardPaneController {
+
+    @FXML
+    JFXDatePicker startDateBox;
+
+    @FXML
+    JFXDatePicker endDateBox;
+
+
+    public void initialize(){
+        //See if we can gray out dates before today's date
+
+    }
+
+
 }
